396e1d8dd22f0edd7e42fcae50cfa9c13e8bbdba
Seraclin <snlin@emory.edu>
2024-02-25 17:10:13 -0500

    Update testPDF.py with Camelot library

--- testPDF.py
+++ testPDF.py
@@ -1,8 +1,17 @@
-# Lib docs: https://github.com/jsvine/pdfplumber
-import pdfplumber
+import pdfplumber  # Lib docs: https://github.com/jsvine/pdfplumber
+import camelot  # https://camelot-py.readthedocs.io/en/master/; For dependencies - https://camelot-py.readthedocs.io/en/master/user/install-deps.html
+import tkinter  # camelot dependency
 import pandas as pd
 
-def extract_tables_from_pdf(pdf_path):
+
+def extract_tables_from_pdf_plumber(pdf_path):
+    """Gets and prints the pdf's tables using pdfplumber
+
+    Parameters
+    ----------
+    pdf_path : str
+        The file location of the pdf
+    """
     with pdfplumber.open(pdf_path) as pdf:
         for page_number in range(len(pdf.pages)):
             page = pdf.pages[page_number]
@@ -22,8 +31,27 @@ def extract_tables_from_pdf(pdf_path):
             else:
                 print(f"No tables found on Page {page_number + 1}")
 
-# Press the green button in the gutter to run the script.
+
+def extract_tables_from_pdf_camelot(pdf_path):
+    """Gets and prints the pdf's tables using camelot-py
+
+    Parameters
+    ----------
+    pdf_path : str
+        The file location of the pdf
+    """
+    tables = camelot.read_pdf(pdf_path, pages='all', strip_text='\n')  # make sure to get rid of newlines
+    print(tables)
+    tables.export('camelot.csv', f='csv')
+
+
+# Testing the performance of various pdf extraction libraries
 if __name__ == '__main__':
     pdf_path = "SOC385Test.pdf"
-    extract_tables_from_pdf(pdf_path)
+    # Note: Camelot > PDFPlumber with default settings
+    print("====PDFPLUMBER====")
+    extract_tables_from_pdf_plumber(pdf_path)
+    print("====CAMELOT====")
+    extract_tables_from_pdf_camelot(pdf_path)
+
 
