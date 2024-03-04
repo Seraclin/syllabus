7af9a0719917bbc31bb980410f2f8d1e3e1537d7
Seraclin <snlin@emory.edu>
2024-03-04 18:06:23 -0500

    Update testPDF.py with block extraction test

--- testPDF.py
+++ testPDF.py
@@ -60,7 +60,8 @@ def extract_tables_from_pdf_pymupdf(pdf_path):
     outName = pdf_path + "_table.csv"
     dataframes = []  # list of DataFrames per table fragment
     for page in doc:  # iterate over the pages
-        tabs = page.find_tables(strategy='lines_strict')  # locate tables on page, by default goes by 'lines' but background images/colors can throw it off
+        tabs = page.find_tables(strategy='lines', snap_tolerance=10)  # locate tables on page, by default goes by 'lines' but background images/colors can throw it off
+        # snap_tolerance = how much y-distance between horizontal lines to be considered separate from each other. (e.g. tall table cells)
         if len(tabs.tables) == 0:  # no tables found?
             print("No table on page", page)  # stop
         else:
@@ -110,24 +111,45 @@ def extract_HTML_from_pdf_pymupdf(pdf_path):
         out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
     out.close()
 
+def extract_blocks_from_pdf_pymupdf(pdf_path):
+    """Gets blocks of pdf's text using pymupdf.
+    Outputs as a list of blocks: (x0, y0, x1, y1, "lines of text in the block", block_no, block_type)
+
+    Parameters
+    ----------
+    pdf_path : str
+        The file location of the pdf
+    """
+    doc = fitz.open(pdf_path)  # open a document
+    # outName = pdf_path+"_block.txt"
+    # out = open(outName, "wb")  # create a text output
+    for page in doc:  # iterate the document pages
+        block = page.get_text(option='blocks')  # blocks
+        for b in block:
+            print(page, ", block", b[5], ":", b[4])
+        # out.write(text)  # write text of page
+        # out.write(bytes((12,)))  # write page delimiter (form feed 0x0C)
+    # out.close()
 
 
 # Testing the performance of various pdf extraction libraries
 if __name__ == '__main__':
     pdf_path = "SOC385Test.pdf"
     # Note: Camelot > PDFPlumber with default settings for tables. However, Camelot doesn't handle any non-table data
-    # print("====PDFPLUMBER====")
+    print("====PDFPLUMBER====")
     # extract_tables_from_pdf_plumber(pdf_path)
-    # print("====CAMELOT====")
+    print("====CAMELOT====")
     # extract_tables_from_pdf_camelot(pdf_path)
     print("====PYMUPDF====")
     # Haven't figured out the table parameters yet....
     # Note: PyMuPDF can handle tables and format to HTML.
     # For text: https://pymupdf.readthedocs.io/en/latest/recipes-text.html#text
     # for tables: https://pymupdf.readthedocs.io/en/latest/page.html#Page.find_tables
-    extract_text_from_pdf_pymupdf(pdf_path)
-    extract_HTML_from_pdf_pymupdf(pdf_path)
-    extract_tables_from_pdf_pymupdf(pdf_path)
+
+    # extract_text_from_pdf_pymupdf(pdf_path)
+    # extract_HTML_from_pdf_pymupdf(pdf_path)
+    # extract_tables_from_pdf_pymupdf(pdf_path)
+    extract_blocks_from_pdf_pymupdf(pdf_path)
 
     print("DONE")
 
