8fabe4e2022346c6efbe29e27953366d1dd9a2e9
Seraclin <snlin@emory.edu>
2024-03-25 19:10:40 -0400

    Update upload button for multiple file inputs
    Also added file size check alert and other error dialogs to warn the user. Note: that files are removed when page is refreshed. Also that files will NOT be uploaded if there is already a matching file in the directory.

--- ReactWebsite/syllabus-main/src/app/api/upload/route.js
+++ ReactWebsite/syllabus-main/src/app/api/upload/route.js
@@ -6,33 +6,49 @@ import { extname, join } from 'path';
 export async function POST(request) {
     // This basically does the same as the previous server implementation but instead via HTTP
     const data = await request.formData()
-    const file = data.get('file')
+    const files = data.getAll('file') // get all files from input
 
     // Error checking
-    if(!file) {
+    if(!files || files.length == 0) {
         // No file found error
         console.error("No file found.")
-        return NextResponse.json({success: false})
+        return NextResponse.json({success: false, error:'No files found.'})
     }
 
-    // Check file extension
-    const fileExtension = extname(file.name).toLowerCase();
-    if (fileExtension !== '.pdf') {
-        console.error("Wrong file type. Please upload a PDF.")
-        return NextResponse.json({ success: false, error: 'File type must be PDF' });
+    // Check file extension, old: single file
+    // const fileExtension = extname(file.name).toLowerCase();
+    // if (fileExtension !== '.pdf') {
+    //     console.error("Wrong file type. Please upload a PDF.")
+    //     return NextResponse.json({ success: false, error: 'File type must be PDF' });
+    // }
+    const successResponses = [];
+    const errorResponses = [];
+    for(const file of files){
+        // Check file extension just to be sure
+        const fileExtension = extname(file.name).toLowerCase();
+        if (fileExtension !== '.pdf') {
+            console.error("Invalid file type. Please upload a PDF.")
+            errorResponses.push({ success: false, error: 'File type must be PDF', fileName: file.name });
+            continue;
+        }
+
+        const bytes = await file.arrayBuffer()
+        const buffer = Buffer.from(bytes)
+
+        // With the file in the buffer, you can do whatever
+        // For now, I'll just write it to the loacal file system
+        // TODO: warning it's bad practice to put user info in the public folder, as anyone can access it
+        // Try to find a secure server to store files rather than locally
+        const rootDir = process.cwd();
+        const path = join(rootDir, 'public', 'tmp', file.name); // TODO change to more secure location
+        await writeFile(path, buffer)
+        console.log(`open ${path} to see the uploaded file`)
+        successResponses.push({ success: true, fileName: file.name });
     }
-
-    const bytes = await file.arrayBuffer()
-    const buffer = Buffer.from(bytes)
-
-    // With the file in the buffer, you can do whatever
-    // For now, I'll just write it to the loacal file system
-    // TODO: warning it's bad practice to put user info in the public folder, as anyone can access it
-    // if they have the file path. Might try instead- https://uploadthing.com/
-    const rootDir = process.cwd();
-    const path = join(rootDir, 'public', 'tmp', file.name);
-    await writeFile(path, buffer)
-    console.log(`open ${path} to see the uploaded file`)
-
-    return NextResponse.json({success: true})
+    // Keep track of all successful and unsuccessful responses for debugging purposes
+    const response = {
+        successResponses,
+        errorResponses
+    };
+    return NextResponse.json(response)
 }
--- ReactWebsite/syllabus-main/src/components/UploadButton.js
+++ ReactWebsite/syllabus-main/src/components/UploadButton.js
@@ -6,27 +6,52 @@ but I use client-side with 'use client' and make an API for HTTP requests;
 The default HTML file upload is kinda hard to customize directly, so you have to hide it and put a label on top 
 */
 
-import { join } from 'path'
+import { join, extname } from 'path'
 import { useState, FormEvent } from 'react'
 
 export default function UploadButton() { // handle upload button behavior
-  const [file, setFile] = useState(null)
+  const [files, setFiles] = useState(null) // FileList array
   const [progress, setProgress] = useState(0) // loading bar
   const [complete, setComplete] = useState(false) // check if completed, true = complete
+  const MAX_FILE_SIZE = 2; // note: file size is measured in bytes in base 2 (1024*1024 bytes = 1 Mb)
 
   const onSubmit = async (e) => {
     setProgress(0);
     setComplete(false);
 
-    // Requires client and API
+    // Check if there's a file
     e.preventDefault()
-    if(!file) {
+    if(!files) {
       console.error("Error: no file provided")
+      alert("No file provided!")
       return;  // Error for no file added
     }
+
+    // Check if multiple input files sum up to less than MAX_FILE_SIZE
+    let totalSize = 0;
+    for(let i = 0; i < files.length; i++) {
+      totalSize += files[i].size;
+      if(extname(files[i].name).toLowerCase() != '.pdf') {
+        console.error("Invalid file type selected. Please upload a PDF.")
+        alert(`Invalid file found: "${files[i].name}". Please select a PDF.`);
+        return;
+      }
+    }
+    if(totalSize > MAX_FILE_SIZE * 1024 * 1024) {
+      console.error("Error: over max file size.")
+      alert(`Total selected files ${(totalSize/1024/1024).toFixed(2)}MB is over the size limit of 2MB.`);
+      return;
+    }
+
+    // Check if correct file type
+
     try {
       const data = new FormData()
-      data.set('file', file);
+      for (let i = 0; i < files.length; i++) {
+        data.append('file', files[i]);
+      }
+
+      // data.set('file', file); // for single file
 
       // XML HTTP requests to track progress
       const xhr = new XMLHttpRequest();
@@ -45,12 +70,13 @@ export default function UploadButton() { // handle upload button behavior
       xhr.onload = async () => {
         if (xhr.status === 200) {
           // setFile(null); // Reset file after successful upload
-          console.log("Upload complete:", file.name)
           // If we reach here, then everything should work
+          console.log("Upload complete:", files)
           setProgress(100);
           setComplete(true);
         } else {
           console.error('Error uploading file:', xhr.statusText);
+          alert("An error has occurred. Please try again.")
           setProgress(0); // Reset progress on error
           setComplete(false);
         }
@@ -58,12 +84,13 @@ export default function UploadButton() { // handle upload button behavior
     
       xhr.onerror = () => {
         console.error('Network error occurred while uploading file');
+        alert("Network error occurred while uploading file. Please try again.")
         setProgress(0); // Reset progress on error
         setComplete(false);
       };
       xhr.send(data);
 
-      /* // This uses HTML fetch instead of xhr, can't really do the loading that well
+      /* // This uses HTML fetch instead of xhr, fetch can't track loading progress
       const res = await fetch('/api/upload', {
         method: 'POST',
         body: data
@@ -105,8 +132,7 @@ export default function UploadButton() { // handle upload button behavior
   }
 
   const handleFileChange = event => { // handle file selction button behavior
-    const selectedFile = event.target.files?.[0];
-    setFile(selectedFile);
+    setFiles(Array.from(event.target.files));
     setProgress(0); // Reset progress and complete state after file selection changes
     setComplete(false);
   }
@@ -117,7 +143,7 @@ export default function UploadButton() { // handle upload button behavior
     <div className='upload-button-container'>
       <form onSubmit={onSubmit}>
         {/* <label htmlFor="file-upload" class="custom-file-upload"> Upload PDF</label> */}
-        <input id="file-upload" type='file' accept='.pdf' name='file' 
+        <input id="file-upload" type='file' accept='application/pdf' name='file' multiple
         onChange={handleFileChange}/>
         <input type='submit' value='Upload' />
       </form>
@@ -128,9 +154,27 @@ export default function UploadButton() { // handle upload button behavior
         <div className='upload-progress-bar-indicator' 
         style={{width:`${progress}%`}} />
       </div>
-      {complete && file ? (<p>Completed upload: {file.name}</p>) : (
-          file ? (<p>Selected file: {file.name}</p>) : (<p>No file selected</p>)
-      )}
+      {complete && files && files.length > 0 ? (
+    <div>
+      <p>Completed uploads:</p>
+      <ul>
+        {files.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
+    </div>) : (
+      files && files.length > 0 ? (
+        <div>
+          <p>Selected files:</p>
+          <ul>
+            {files.map((file, index) => (
+              <li key={index}>{file.name}</li>
+            ))}
+          </ul>
+        </div>) : (
+        <p>No files selected</p>
+      )
+    )}
     </div>
     </>
   )
