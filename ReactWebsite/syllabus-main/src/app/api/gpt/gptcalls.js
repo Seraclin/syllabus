bbbb04c1954631de3558633621dbbd7e3d92cdae
Gianluis Hernandez <greenflare10@gmail.com>
2024-03-31 23:26:16 -0400

    Button
    There's a button on screen. it does nothing. it'll eventually do something, but I must check the main branch rn so

--- ReactWebsite/syllabus-main/src/app/api/gpt/gptcalls.js
+++ ReactWebsite/syllabus-main/src/app/api/gpt/gptcalls.js
@@ -1,6 +1,20 @@
 import OpenAI from "openai";
-import apiKey from './key.js';
+import apiKey from './key';
+
+import { writeFile } from 'fs/promises'
+import { NextRequest, NextResponse } from "next/server";
+import { extname, join } from 'path';
+
 
 const openai = new OpenAI({
-    organization: apiKey,
+    apiKey: apiKey, // This is the default and can be omitted
 });
+
+async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-3.5-turbo',
+    });
+    console.log(chatCompletion);
+}
+
--- ReactWebsite/syllabus-main/src/app/page.js
+++ ReactWebsite/syllabus-main/src/app/page.js
@@ -1,5 +1,6 @@
 /* The Homepage, view locally with npm run dev, delete .next folder locally if not working */
 import UploadButton from "@/components/UploadButton"
+import gptcalls from "@/app/api/gpt/gptcalls"
 export default function Home() {
   // const uploadFile = function(file) {
   //   const test = file.target.files[0];
@@ -12,14 +13,21 @@ export default function Home() {
 
   return (
     <>
-      <header className="title-bar">
-        <h1 className='title-bar-text'>Master Syllabus (in progress)</h1>
-        <a href="https://github.com/Seraclin/syllabus">About</a>
-      </header>
-      <p>Please upload a PDF to get started. TODO: Insert something cool here :)</p>
-      <div>
-        <UploadButton />      
-      </div>
+        <header className="title-bar">
+            <h1 className='title-bar-text'>Master Syllabus (in progress)</h1>
+            <a href="https://github.com/Seraclin/syllabus">About</a>
+        </header>
+        <p>Please upload a PDF to get started. TODO: Insert something cool here :)</p>
+        <div>
+            <UploadButton />      
+        </div>
+        <form>
+            <input class="styled" type="button" value="dick and balls" />
+          </form>
+          <p>
+            <gptcalls />
+          </p>
+      
     </>
   )
 }
