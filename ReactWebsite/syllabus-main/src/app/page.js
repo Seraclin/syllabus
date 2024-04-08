7292c387cc198caeb2228dd0f2085bd2d2dece29
Seraclin <snlin@emory.edu>
2024-04-07 21:33:20 -0400

    Added dummy ads to page

--- ReactWebsite/syllabus-main/src/app/page.js
+++ ReactWebsite/syllabus-main/src/app/page.js
@@ -24,8 +24,21 @@ export default function Home() {
       <div>
         <UploadButton />
         <DownloadButton fileName="test1.ics"/>
-        <CalendarEvent></CalendarEvent>
       </div>
+      <div className="container1">
+        <div className="ad-container left-justified">
+          <img src="/Ad1.jpg" alt="Ad 1"></img>
+          <img src="/Ad2.jpg" alt="Ad 2"></img>
+          <img src="/Ad3.jpg" alt="Ad 3"></img>
+        </div>
+        <CalendarEvent></CalendarEvent>
+        <div className="ad-container right-justified">
+          <img src="/Ad4.jpg" alt="Ad 4"></img>
+          <img src="/Ad5.jpg" alt="Ad 5"></img>
+          <img src="/Ad6.jpg" alt="Ad 6"></img>
+        </div>
+      </div>
+      
     </>
   )
 }
