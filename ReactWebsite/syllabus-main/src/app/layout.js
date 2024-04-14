af77a00186c9ddfb54167e39f53a9dd810265268
Seraclin <snlin@emory.edu>
2024-04-14 15:54:50 -0400

    Fixed previous commit deleting my changes. Added logo
    The ads got deleted along with some other CSS. Also added the logo back and user files list.

--- ReactWebsite/syllabus-main/src/app/layout.js
+++ ReactWebsite/syllabus-main/src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Master Syllabus",
-  description: "Master Syllabus allows you to easily organize your syllabi with ease. Just upload a PDF.",
+  title: "eSyllabus",
+  description: "eSyllabus allows you to easily organize your syllabi with ease. Just upload a PDF.",
 };
 
 export default function RootLayout({ children }) {
--- ReactWebsite/syllabus-main/src/app/page.js
+++ ReactWebsite/syllabus-main/src/app/page.js
@@ -4,6 +4,7 @@ import CalendarEvent from "@/components/CalendarEvent"  // TODO delete this
 import DownloadButton from "@/components/DownloadButton"  // TODO edit this
 import { getSession,logout } from "@/_utils/lib";
 import { redirect } from "next/navigation";
+import UserFilesList from "@/components/UserFiles";
 
 
 
@@ -12,6 +13,7 @@ export default async function Home() {
   return (
     <>
       <header className="title-bar">
+        <img src="/eSyllabus.png" style={{height: '100px', width:'100px'}}></img>
         <div className="title-container">
           <h1 className='title-bar-text'>eSyllabus</h1>
         </div>
@@ -44,8 +46,21 @@ export default async function Home() {
       <p>Please upload a PDF to get started.</p>
       <div>
         <UploadButton />
+        <UserFilesList />
         <DownloadButton fileName="test1.ics"/>
-        <CalendarEvent></CalendarEvent>
+        <div className="container1">
+          <div className="ad-container left-justified">
+            <img src="/Ad1.jpg" alt="Ad 1"></img>
+            <img src="/Ad2.jpg" alt="Ad 2"></img>
+            <img src="/Ad3.jpg" alt="Ad 3"></img>
+          </div>
+          <CalendarEvent></CalendarEvent>
+          <div className="ad-container right-justified">
+            <img src="/Ad4.jpg" alt="Ad 4"></img>
+            <img src="/Ad5.jpg" alt="Ad 5"></img>
+            <img src="/Ad6.jpg" alt="Ad 6"></img>
+          </div>
+        </div>
       </div>
     </>
   )
