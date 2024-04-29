5180f197c4ae69b4f85e4d61b814a2297e3b8488
Hridansh Saraogi <78390395+HridanshS@users.noreply.github.com>
2024-04-28 20:16:03 -0400

    UI changes for the sign-up page

--- ReactWebsite/syllabus-main/src/app/signup/page.js
+++ ReactWebsite/syllabus-main/src/app/signup/page.js
@@ -1,24 +1,23 @@
 import { redirect } from "next/navigation";
 import { getSession, signup } from "@/_utils/lib";
+import "../globals.css"; // Adjust the path according to your file structure
 
 export default async function Page() {
   return (
-    <section>
-        <h1>Signup</h1>
+    <section className="signup-section">
+      <h1>Signup</h1>
       <form
+        className="signup-form"
         action={async (formData) => {
           "use server";
           await signup(formData);
           redirect("/");
         }}
       >
-        <input type= "text" placeholder="Name" name="name" id="name" />
-        <br />
+        <input type="text" placeholder="Name" name="name" id="name" />
         <input type="email" placeholder="Email" name="email" id="email" />
-        <br />
         <input type="password" placeholder="Password" name="password" id="password" />
-        <br/>
-        <button type="submit">signup</button>
+        <button type="submit">Signup</button>
       </form>
     </section>
   );
